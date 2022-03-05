c238805a8b16c672f16f84ae7988d14fd008de26
AdaWang33 <vivianqqy@gmail.com>
2022-03-05 14:15:16 -0500

    update part1

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,94 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+
+const Button = ({clickEvent, text}) => {
+  return (
+    <button onClick={clickEvent}>{text}</button>
+  )
+} 
+
+const StatisticLine = ({text, value}) => {
+  return (
+    <tr>
+      <td> {text} </td>
+      <td> {value}</td>
+    </tr>
+  )
+}
+  
+const Statistics = ({count}) => {
+  const all = count.good+count.neutral+count.bad;
+  if (all===0) {
+    return <div>No feedback given.</div>
+
+  } else {
+    const average = count.good-count.bad;
+    let positive;
+    if (all===0) {
+      positive = 0;
+    } else {
+      positive = count.good/all;
+    }
+    return (
+      <table>
+        <tbody>
+        <StatisticLine text='good' value={count.good}></StatisticLine>
+        <StatisticLine text='neutral' value={count.neutral}></StatisticLine>
+        <StatisticLine text='bad' value={count.bad}></StatisticLine>
+        <StatisticLine text='all' value={all}></StatisticLine>
+        <StatisticLine text='average' value={average}></StatisticLine>
+        <StatisticLine text='positive' value={positive*100 + '%'}></StatisticLine>
+        </tbody>
+      </table>   
+    );
+  }
+
+}
+
+
+const App = () => {
+
+  const [count, setCount] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0
+  });
+
+  const handleGood = () => {
+    setCount({
+      ...count,
+      good: count.good + 1
+    })
+  }
+
+  const handleNeutral = () => {
+    setCount({
+      ...count,
+      neutral: count.neutral + 1
+    })
+  }
+
+  const handleBad = () => {
+    setCount({
+      ...count,
+      bad: count.bad + 1
+    })
+  }
+
+  return (
+    <div className="App">
+      <header>give feedback</header>
+      <div className="buttons">
+        <Button clickEvent={handleGood} text='good'></Button>
+        <Button clickEvent={handleNeutral} text='neutral'></Button>
+        <Button clickEvent={handleBad} text='bad'></Button>
+      </div>
+      <p id="statistics"> statistics </p>
+      <Statistics count={count}></Statistics>
+    </div>
+  );
+}
+
+export default App;
